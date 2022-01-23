34adbe04320374e13842e303d672f296478c5fbb
redryo1505 <redryo1505@gmail.com>
2022-01-23 14:21:13 +0700

    crawling

--- crawl_data.go
+++ crawl_data.go
@@ -5,7 +5,24 @@ import (
 	"time"
 )
 
+func crawl(number int, nameOfRoutine int) {
+	fmt.Printf("Routine %d Crawling %d \n", nameOfRoutine, number)
+}
 func main() {
-	fmt.Println("hello")
+
+	const numberOfRequests int = 1000
+	data := make(chan int, numberOfRequests)
+	defer close(data)
+	for i := 1; i <= numberOfRequests; i++ {
+		data <- i
+	}
+	for i := 1; i <= 5; i++ {
+		go func(nameOfRoutine int) {
+			for value := range data {
+				crawl(value, nameOfRoutine)
+			}
+			fmt.Printf("Routine %d is done\n", nameOfRoutine)
+		}(i)
+	}
 	time.Sleep(time.Second)
 }
